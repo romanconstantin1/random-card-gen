5b52a7d3f2a4033864edb0ff463cff4930e187cd
romanconstantin1 <romanconstantin1@gmail.com>
2023-01-23 19:20:50 +0000

    functional prototype! random functions & color generation

--- src/app.js
+++ src/app.js
@@ -2,7 +2,30 @@
 import "bootstrap";
 import "./style.css";
 
-
+const suitList = ["♦", "♥", "♠", "♣"];
+const suitCol = ["black", "red"];
+const cardVal = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
 window.onload = function() {
-
+  let suitGen = Math.floor(Math.random() * 4);
+  let colGen = Math.floor(Math.random() * 2);
+  let valGen = Math.floor(Math.random() * 13);
+  document.querySelector(".top-left").innerHTML = `${suitList[suitGen]}`;
+  document.querySelector(".top-left").style = `color: ${suitCol[colGen]}`;
+  document.querySelector(".center-text").innerHTML = `${cardVal[valGen]}`;
+  document.querySelector(".bottom-right").innerHTML = `${suitList[suitGen]}`;
+  document.querySelector(".bottom-right").style = `color: ${suitCol[colGen]}`;
 };
